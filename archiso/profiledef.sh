b9d8fc4dd50c0fe790391f1d56a0a6c8eded3c35
Dodoprospy3 <dodoprospy3@gmail.com>
2026-09-08 14:06:17 +0300

    Add live-session scripts, complete syslinux/UEFI boot menus, and full package list

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -4,7 +4,7 @@
 iso_name="oozylinux"
 iso_label="OOZY_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Oozy Linux <https://github.com/Dodoprospy3/Oozy-Linux>"
-iso_application="Oozy Linux Live/Rescue/Installation Medium"
+iso_application="Oozy Linux Live/Install Medium"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
@@ -13,12 +13,17 @@ bootmodes=('bios.syslinux'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86,arm64' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/oozy-live-setup"]="0:0:755"
   ["/usr/local/bin/oozy-welcome"]="0:0:755"
+  ["/usr/local/bin/calamares-launch"]="0:0:755"
 )
